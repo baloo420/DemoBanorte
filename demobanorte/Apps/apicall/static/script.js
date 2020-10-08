4d9ebf3b71ba4f4a41581e7227c080b3573ab46e
Hugo Mendieta <hugo.mendieta.pacheco@gmail.com>
2020-10-08 15:38:58 -0500

    Estilos menus

--- demobanorte/Apps/apicall/static/script.js
+++ demobanorte/Apps/apicall/static/script.js
@@ -3,20 +3,48 @@ window.onload = function() {
 	//console.log(eliminocta);
 	if (typeof(eliminocta) != 'undefined' && eliminocta != null){
 		var tarjeta = document.getElementsByClassName("tarjeta");
-		var button  = document.getElementById('eliminocta_btn');
+		var buttonCta  = document.getElementById('eliminocta_btn');
+		var buttonCons  = document.getElementById('eliminoconsent_btn');
 		//console.log("hidden: " + button.classList.contains("hidden"));
 		if(tarjeta.length==0){
 			//document.getElementById('eliminocta_btn').add
-			if(!button.classList.contains("hidden")){
-				button.classList.add("hidden");
+			if(!buttonCta.classList.contains("hidden")){
+				buttonCta.classList.add("hidden");
+			}
+			if(!buttonCons.classList.contains("hidden")){
+				buttonCons.classList.add("hidden");
 			}
 		}else{
 			//document.getElementById('eliminocta_btn').add
-			if(button.classList.contains("hidden")){
-				button.classList.remove("hidden");
+			if(buttonCta.classList.contains("hidden")){
+				buttonCta.classList.remove("hidden");
+			}
+			if(buttonCons.classList.contains("hidden")){
+				buttonCons.classList.remove("hidden");
 			}
 		}
 	}
+
+
+
+	var menus = document.getElementsByClassName('main-menu');
+	for(var i=0; i<menus.length; i++){
+		if(String(menus[i].href).search(window.location.pathname)>=0){
+			menus[i].classList.add('active');
+		}
+	}
+	//console.log(window.location.pathname);
+
+
+
+
+
+	var tarjeta = document.getElementsByClassName('tarjeta');
+	console.log("Tarjeta: "+tarjeta);
+	tarjeta.addEventListener('click', function(event){
+		console.log("Tarjeta click");
+	});
+
 }
 
 
